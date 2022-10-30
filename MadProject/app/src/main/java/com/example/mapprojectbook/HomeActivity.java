72b7214cccf9344e781fd204b62c93dd94a03cf6
sappy1944 <sappy1944@users.noreply.github.com>
2022-10-30 11:53:51 +0530

    Changes

--- MadProject/app/src/main/java/com/example/mapprojectbook/HomeActivity.java
+++ MadProject/app/src/main/java/com/example/mapprojectbook/HomeActivity.java
@@ -1,15 +1,35 @@
 package com.example.mapprojectbook;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mapprojectbook.databinding.ActivityHome2Binding;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,53 +37,89 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseDatabase db;
     private List<Phone> phoneList;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityHome2Binding binding;
+    private DrawerLayout drawer;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home2);
 
-        RecyclerView recyclerView = findViewById(R.id.recycler_view_phone);
-        recyclerView.setLayoutManager((new LinearLayoutManager(this)));
-        recyclerView.setHasFixedSize(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        final PhoneAdapter adapter = new PhoneAdapter();
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_phone);
+        recyclerView.setLayoutManager((new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)));
+
+        PhoneAdapter adapter = new PhoneAdapter();
         recyclerView.setAdapter(adapter);
 
         phoneList = new ArrayList<Phone>();
-        db.getInstance().getReference().child("Phones").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    phoneList.clear();
-                    for(DataSnapshot ss:snapshot.getChildren()) {
-                        Phone phone = ss.getValue((Phone.class));
-                        phoneList.add(phone);
-                    }
-                    adapter.notifyDataSetChanged();
-                }
-            }
+        Phone phone = new Phone("user@gmail.com", "phone", "desc", 5000.0, "new", 1);
+        phoneList.add(phone);
+        phoneList.add(phone);
+        phoneList.add(phone);
+        phoneList.add(phone);
+        phoneList.add(phone);
+//        db.getInstance().getReference().child("Phones").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (snapshot.exists()) {
+//                    phoneList.clear();
+//                    for (DataSnapshot ss : snapshot.getChildren()) {
+//                        Phone phone = ss.getValue((Phone.class));
+//                        phoneList.add(phone);
+//                    }
+//                    adapter.setPhones(phoneList);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
+        adapter.setPhones(phoneList);
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_home:
+                startActivity(new Intent(this, HomeActivity.class));
+                break;
+            case R.id.nav_profile:
+                startActivity((new Intent(this, ProfileActivity.class)));
+                break;
+        }
+        return true;
+    }
 
-
-
-
-
-
-
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen((GravityCompat.START))) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
 }
--- MadProject/app/src/main/java/com/example/mapprojectbook/Phone.java
+++ MadProject/app/src/main/java/com/example/mapprojectbook/Phone.java
@@ -2,6 +2,8 @@ package com.example.mapprojectbook;
 
 public class Phone {
 
+    private int image;
+
     private String seller_email;
 
     private String phone_name;
@@ -12,16 +14,21 @@ public class Phone {
 
     private String condition;
 
-    public Phone(String seller_email, String phone_name, String description, Double price, String condition) {
+    public Phone(String seller_email, String phone_name, String description, Double price, String condition, int image) {
         this.seller_email = seller_email;
         this.phone_name = phone_name;
         this.description = description;
         this.price = price;
         this.condition = condition;
+        this.image = image;
     }
 
     public String getSeller_email() {
         return seller_email;
+    }
+
+    public int getImage() {
+        return image;
     }
 
     public String getPhone_name() {
